cabc37745c0949b86ab68f11e193bbb362e1e92c
Ronny Wolf <rwolf@bluecatnetworks.com>
2019-05-29 14:00:05 +0200

    Added a couple of lines

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 
 import time
 import json
+import configparser
 
 def setup():
     print("")
@@ -16,7 +17,8 @@ def setup():
     print("3) Configure Microsoft Azure")
     print("4) Configure Google Cloud Platform")
     print("7) Install prerequisits")
-    print("8) Create Bluecat Gateway Workflow")
+    print("8) Install as cronjob")
+    print("9) Create Bluecat Gateway Workflow")
     print("0) Exit application")
     option = str(input("Please enter option: "))
     if len(option) == 1:
@@ -33,11 +35,27 @@ def switch(option):
         bam_ip = str(input("Please enter your BlueCat Address Manager IP address: "))
         bam_api_user = str(input("Please enter your API user account: "))
         bam_api_password = str(input("Please enter your API user password: "))
-        with open('config.json'. 'w') as outfile:
-            json.dump(bam_ip, outfile)
-        print(bam_ip)
-        print(bam_api_user)
-        print(bam_api_password)
+        config_write("test.json", "data")
+    elif option == "2":
+        aws_region = str(input("Please enter your AWS region: "))
+        aws_id = str(input("Please enter your AWS access key: "))
+        aws_secretkey = str(input("Please enter your AWS secret key: "))
+        config_write("test.json", "data")
+    elif option == "3":
+        azure_subscription = str(input("Please enter your Azure subscription: "))
+        azure_client = str(input("Please enter your Azure client id: "))
+        azure_client_secret = str(input("Please enter your Azure client secret: "))
+        azure_tenant_id = str(input("Please enter your Azure tenant id: "))
+        config_write("test.json", "data")
+    elif option == "4":
+        gcp_config_file = str(input("Please enter the path and filename to the GCP json file: "))
+        config_write("test.json", "data")
 
+
+def config_write(configfile, data):
+    print("Do some work")
+
+def config_read(configfile, data):
+    print("Do some other work")
      
 setup()
